2dbc93d021e5a65508978407aae365c2c5ab2f37
Dev2 <Dev2@gmail.com>
2024-07-17 00:20:24 +0530

    Dev01 modified Test.java

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -1,9 +1,13 @@
 package com.nt.test;
 
+import com.nt.beans.Utility;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Utility u=new Utility();
+		System.out.println("System Date= "+u.getSysDate());
 
 	}
 
