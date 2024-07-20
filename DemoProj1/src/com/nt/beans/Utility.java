62eae45a476a8cc2c4f2a13408df2b3d03e54a4d
Dev2 <Dev2@gmail.com>
2024-07-20 10:08:30 +0530

    added

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -10,7 +10,7 @@ public class Utility {
 
 	public void m1() {
 
-		System.out.println("Utility m1() method:abcdefgh");
+		System.out.println("Utility m1() method");
 
 		System.out.println("Vimlesh Kuamr Singh");
 
