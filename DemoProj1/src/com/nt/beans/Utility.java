fd54f79f753834d1fbb1e6614baa1f404df1e683
Dev2 <Dev2@gmail.com>
2024-07-20 09:38:20 +0530

    dummy commit

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -9,6 +9,8 @@ public class Utility {
         	  return new  Date();
        }
           public void m1() {
+        	  
+        	  
 
         	  System.out.println("Utility m1() method:abcdefgh");
 
