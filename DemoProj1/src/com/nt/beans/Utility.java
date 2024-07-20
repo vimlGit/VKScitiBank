4d68ad33bfefd634412cf86caa14a943fb00595d
Dev2 <Dev2@gmail.com>
2024-07-20 07:38:18 +0530

    TL Modified u.java

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -9,7 +9,7 @@ public class Utility {
         	  return new  Date();
        }
           public void m1() {
-        	  System.out.println("Utility m1() method");
+        	  System.out.println("Utility m1() method: 123456789");
           }
 	
 
